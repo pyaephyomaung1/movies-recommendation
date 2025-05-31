09ab2e717a1349d7ad17a0861ea2d29da8801768
Pyae Phyo Maung <pyaephyomg.ppm06@gmail.com>
2025-06-01 01:22:50 +0700

    Project Setup

--- backend/src/main/java/com/store/backend/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.store.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.store.backend.models.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer>{
-    
-}
--- backend/src/main/java/com/store/backend/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.store.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.store.backend.models.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer >{
-    
-}
