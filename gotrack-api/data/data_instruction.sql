2419d6febc0af404ddb9520c439e40acf766ab09
Pyae Phyo Maung <ppm@Pyaes-MacBook-Pro.local>
2025-07-02 03:03:41 +0700

    Changing to movies

--- gotrack-api/data/data_instruction.sql
+++
@@ -1,3 +0,0 @@
-SELECT t.*, a.artist_id, a.band_id
-FROM track t
-JOIN album a ON t.album_id = a.id;
--- gotrack-api/src/main/java/com/gotrack/gotrack_api/repository/AlbumRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gotrack.gotrack_api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gotrack.gotrack_api.model.Album;
-
-public interface AlbumRepository extends JpaRepository<Album, Integer>{
-    
-}
